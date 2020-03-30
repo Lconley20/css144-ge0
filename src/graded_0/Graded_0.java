3e203278221b16fcd863ef269269b262a3a192bb
ljc98 <ljc98@DESKTOP-QOVDADE.attlocal.net>
2020-03-30 01:37:45 -0500

    Fixed methods to reflect assignment

--- src/graded_0/Graded_0.java
+++ src/graded_0/Graded_0.java
@@ -16,35 +16,66 @@ import java.util.Random;
  * @author Logan Conley
  */
 public class Graded_0 {
-    public static void randomList() {
-        Random rand = new Random();
     
-        int check = 10 + rand.nextInt(90);
+    public static List<Integer> randomList() {
+        
+        Random rand = new Random();
+            
         int length = rand.nextInt(20);
-        List random = new ArrayList();
+        List<Integer> random = new ArrayList<>();
                 
         for (int i = 0; i < length; i++) {
             int num = 10 + rand.nextInt(90);
-            System.out.println(num);
             random.add(num);
         }
         
-        boolean exists = random.contains(check);
+        boolean sorted = false;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < length - 1; i++) {
+                if (random.get(i) > random.get(i+1)) {
+                    int temp = random.get(i);
+                    int index = random.indexOf(temp);
+                    random.set(index, random.get(i+1));
+                    random.set((index +1), temp);
+                    sorted = false;
+                }
+            }
+        }
         
+        return random;
+    }
+    
+    public static void linear(List<Integer> l) {
+        
+        Random rand = new Random();
+        int check = 10 + rand.nextInt(90);
+        
+        boolean exists = l.contains(check);
         if (exists == true) {
-            System.out.println("The list contains " + check + " at:" + random.indexOf(check));
+            System.out.println("The list contains " + check + " at: " + l.indexOf(check));
         } else {
             System.out.println("List does not contain " + check);
         }
     }
+    
+    public static void binary() {
         
+        Random rand = new Random();
+        int check = 10 + rand.nextInt(90);
+        
+        
+        
+        
+    }   
         
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        randomList();
+        List<Integer> list = randomList();
+        linear(list);
     }
     
 }
